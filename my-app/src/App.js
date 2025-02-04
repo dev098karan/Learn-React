67ea738d21ae6ed524b8a6a498fe3958ab5f23f0
dev098karan <75852041+dev098karan@users.noreply.github.com>
2025-02-04 19:06:14 +0530

    Use of 'props' in React Component
    Use of 'props' in React Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ClassComponent from './components/ClassComponent';
 import FunctionComponent from './components/FunctionComponent';
+import PropsInComponent from './components/PropsInComponent';
 
 function App() {
   return (
     <div className="App">
       <ClassComponent />
       <FunctionComponent />
+      <PropsInComponent property="World" />
     </div>
   );
 }
---
+++ my-app/src/components/PropsInComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PropsInComponent(props) {
+    return <h1>I am a Component containing Property = {props.property}</h1>;
+}
+
+export default PropsInComponent;
