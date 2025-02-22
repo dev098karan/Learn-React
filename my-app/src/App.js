e5f4dbd5716daf7d19ac871d1253014643b99af2
Karan Dev <dev098karan@gmail.com>
2025-02-22 19:39:48 +0530

    Use of 'props' in Constructor

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import FunctionComponent from './components/FunctionComponent';
 import PropsInComponent from './components/PropsInComponent';
 import ComponentInComponent from './components/ComponentInComponent';
 import ConstructorInComponent from './components/ConstructorInComponent';
+import PropsInConstructor from './components/PropsInConstructor';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <PropsInComponent property="World" />
       <ComponentInComponent />
       <ConstructorInComponent />
+      <PropsInConstructor property="World" />
     </div>
   );
 }
